285af752bdf38b6e7b6803cbeb75ca1bb98d285a
JDDParsons <49915859+JDDParsons@users.noreply.github.com>
2026-04-10 11:47:04 -0400

    new icon

--- finance-app/nuxt.config.ts
+++ finance-app/nuxt.config.ts
@@ -10,12 +10,12 @@ export default defineNuxtConfig({
     baseURL: '/Finance-App/',
     head: {
       link: [
-        { rel: 'apple-touch-icon', href: '/Finance-App/coin3.png' },
+        { rel: 'apple-touch-icon', href: '/Finance-App/cathcart_studio_ghibli2.png' },
       ],
       meta: [
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
         { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
-        { name: 'apple-mobile-web-app-title', content: 'Affiance' },
+        { name: 'apple-mobile-web-app-title', content: 'R&J Home Finance' },
       ],
     },
   },
